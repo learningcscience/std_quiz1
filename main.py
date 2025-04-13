a8494584a290eae096dd79cccda56ea24b948269
Sanjay Giri <sanjaygir@gmail.com>
2025-04-13 15:59:23 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from util import *
+
+
+
+
+a = 23
+
+
+if is_prime(a):
+    print(f"{a} is a prime number.")
+else:
+    print(f"{a} is not a prime number.")
+
+
+
+    
+            
---
+++ util.py
@@ -0,0 +1,59 @@
+#function to find if a given number is prime or not
+#assigned: mohan
+def is_prime(n):
+    pass
+
+
+#function to rotate a given string n times
+#assigned: mahendra
+def rotate_string(s, n):
+    pass
+
+
+
+#function to find the factorial of a given number
+#assigned: bastola
+def factorial(n):
+    pass
+
+
+#function to find the nth Fibonacci number
+#assigned: mohan
+def fibonacci(n):
+    pass
+
+
+#function to find the GCD of two numbers
+#assigned: mahendra
+def gcd(a, b):
+    pass
+
+
+#function to find the LCM of two numbers
+#assigned: bastola
+def lcm(a, b):
+    pass
+
+
+#function to find the square root of a given number
+#assigned: mohan
+def square_root(n):
+    pass
+
+
+#function to find the sum of digits of a given number
+#assigned: mahendra
+def sum_of_digits(n):
+    pass
+
+#function to find the reverse of a given string
+#assigned: bastola
+def reverse_string(s):
+    pass
+
+
+
+
+
+
+
