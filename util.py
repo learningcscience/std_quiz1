5cdbbd87395db12f40c02fbf9a85c6e24d238d19
mahend0947D <website0947d@gmail.com>
2025-04-15 13:35:16 +0545

    Update util.py
    i add codes in revrese  Nth time , GCD,  SUM OF DIGITS

--- util.py
+++ util.py
@@ -7,7 +7,21 @@ def is_prime(n):
 #function to rotate a given string n times
 #assigned: mahendra
 def rotate_string(s, n):
-    pass
+ 
+    s = list(s)  
+    t= ""
+    r = ""  
+    c=""
+
+    for i in range(n):
+        if len(s) > 0:
+            t = s.pop()  
+            s.insert(0,t)
+            
+            c= "".join(s)
+
+         
+    return c
 
 
 
@@ -26,7 +40,14 @@ def fibonacci(n):
 #function to find the GCD of two numbers
 #assigned: mahendra
 def gcd(a, b):
-    pass
+    if a<b:
+       i=a
+    else:
+        i=b
+    for i in range(i,0,-1):
+        if a%i==0 and b%i==0:
+            return i
+        
 
 
 #function to find the LCM of two numbers
@@ -44,7 +65,16 @@ def square_root(n):
 #function to find the sum of digits of a given number
 #assigned: mahendra
 def sum_of_digits(n):
-    pass
+    convertstr = str(n)  
+    sum = 0
+    for i in convertstr:
+        c = int(i)        
+        sum += c         
+    return sum
+
+
+
+
 
 #function to find the reverse of a given string
 #assigned: bastola
